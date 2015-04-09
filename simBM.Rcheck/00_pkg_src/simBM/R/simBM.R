d03c5324445eebd8f29855d1f366106e420e1bd4
Yuan Li <li438@wisc.edu>
2015-04-09 13:06:41 -0500

    Add plot function

--- simBM.Rcheck/00_pkg_src/simBM/R/simBM.R
+++ simBM.Rcheck/00_pkg_src/simBM/R/simBM.R
@@ -13,3 +13,15 @@ simBM <-
 	x <- matrix(rnorm(2*n,0,sigma),ncol=2)
 	apply(x,2,cumsum)
 }
+
+##' Plot BM
+##' @param x A matrix of Brownian motion data
+##' @return Brownina motion simulation path figure
+##' @examples x <- simBM(1000,1)
+##' plotBM(x)
+##' @export
+plotBM <- function(x){
+    plot(x,main = "Brownian Motion Simulation Path", xlab = "X", ylab ="Y",col="green")
+    points(x[1,1],x[1,2])
+    points(x[nrow(x),1],x[nrow(x),2],pch=0)
+  }
